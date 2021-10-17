1e7495cd6c3eef9d580ec7d084e60c780b901261
maxpanec <56574560+maxpanec@users.noreply.github.com>
2021-10-17 04:23:30 -0700

    Remove leftover print statements

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flaskapp.sql_connection import Connection
 
+#create flask server
 app = Flask(__name__)
-
 app.secret_key = '04360346034986dr0'
 
+#create database connection
 con = Connection()
 con.sqlConnect()
 
+#setup url routes
 from .routes import main
 app.register_blueprint(main)
