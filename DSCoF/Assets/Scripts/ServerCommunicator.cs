f226c277e54cd90d1b795581a25ad833e8a1317e
jalenng <jalen_ng@outlook.com>
2021-10-17 00:54:17 -0700

    Update server communication script

--- DSCoF/Assets/Scripts/ServerCommunicator.cs
+++ DSCoF/Assets/Scripts/ServerCommunicator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -28,7 +29,6 @@ public class ServerCommunicator : MonoBehaviour
         Debug.Log("Testing Get...");
         int[] ids = {1, 2, 3, 4, 5};
         StartCoroutine(GetResponse(ids));
-
         
         Debug.Log("Testing Post...");
         StartCoroutine(PostResponse(1, 'A'));
@@ -36,12 +36,20 @@ public class ServerCommunicator : MonoBehaviour
  
     IEnumerator PostResponse(int id, char choice) {
         // Create post data
-        WWWForm data = new WWWForm();
-        data.AddField("id", id.ToString());
-        data.AddField("choice", choice.ToString().ToUpper());
+        string bodyJsonString = string.Format(
+            "{{\"id\": \"{0}\", \"choice\", \"{1}\"}}", 
+            id.ToString(), 
+            choice.ToString().ToUpper());
 
         // Create and send request
-        UnityWebRequest request = UnityWebRequest.Post(url, data);
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+
+        request.SetRequestHeader("Content-Type", "application/json");
+
         yield return request.SendWebRequest();
  
         // Check for error
@@ -58,7 +66,7 @@ public class ServerCommunicator : MonoBehaviour
         // Create and send request
         string joinedIds = string.Join(",", ids);
         string query = string.Format("?id={0}", joinedIds);
-        UnityWebRequest request = UnityWebRequest.Get(url + query);
+        UnityWebRequest request = new UnityWebRequest(url + query, "POST");
         yield return request.SendWebRequest();
  
         // Check for error
